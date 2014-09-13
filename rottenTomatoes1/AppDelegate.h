b6111f320da88e30b31b6c682ecfe28a029ed61d
Rattan Priya <rpriya@yahoo-inc.com>
2014-09-12 17:58:29 -0700

    Initial Commit

---
+++ rottenTomatoes1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  rottenTomatoes1
+//
+//  Created by Rattan Priya on 9/12/14.
+//  Copyright (c) 2014 Priya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ rottenTomatoes1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  rottenTomatoes1
+//
+//  Created by Rattan Priya on 9/12/14.
+//  Copyright (c) 2014 Priya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
